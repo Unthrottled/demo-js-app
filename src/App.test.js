1bb1e87e9ea9336f3b97b65162006bb54b357697
Alex Simons <alexsimons9999@gmail.com>
2020-08-21 19:13:09 -0500

    Added skippo

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,15 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', function () {
+
+  test('renders learn react link', () => {
+    const {getByText} = render(<App/>);
+    const linkElement = getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('really long wait time', ()=> {
+    return new Promise(res => setTimeout(res, 10000));
+  });
 });
